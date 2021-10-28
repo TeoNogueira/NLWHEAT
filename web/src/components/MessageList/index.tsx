87ade817253175dac63d2293ff6a83cd90296e18
Teo Nogueira <teonogueira17@gmail.com>
2021-10-27 21:53:26 -0300

    stopped:1:01:27_beginApiToBackEndNode+yarndev_in_backendStart_yarnPrismaStudio_In_backend

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,8 +1,58 @@
+import styles from './styles.module.scss';
+import { api } from '../../services/api'
+import logoImg from '../../assets/logo.svg'
+import { useEffect, useState } from 'react';
+
+type Message = {
+
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
+
+
 export function MessageList() {
 
+const [messages, setMessages] = useState<Message[]>([] )
+
+//Para fazer uma requisição pro backend use o useEffect
+
+useEffect(() => {
+
+    api.get<Message[]>('messages/last3').then(response => {
+
+        setMessages(response.data);
+    })
+
+}, []);
 
     return (
+        
+   <div className={styles.messageListWrapper}>
+       <img src={logoImg} alt="DoWhile 2021" />
+     
+       <ul className={styles.messageList}>
+       
+       {messages.map(message => {
 
-        <h1>Hello World!</h1>
+            return (
+
+        <li key={message.id} className={styles.message}>
+            <p className={styles.messageContent}>{message.text}</p>
+            <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                    <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+            </div>
+        </li>
+        );
+    })}
+       </ul>
+   </div>
+       
     )
 }
