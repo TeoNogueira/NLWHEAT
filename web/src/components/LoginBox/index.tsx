39e9a5b733b2947ae03f04fee30c77f2fcec467c
Teo Nogueira <teonogueira17@gmail.com>
2021-10-30 13:04:26 -0300

    1:27:51_ContextAuthForAllComponents_and_createAuthPersistentInLocalStorage_etc...

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -2,65 +2,20 @@
 import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
 
-import { useEffect } from 'react';
-import { api } from '../../services/api';
+import { useContext } from 'react';
+// import { api } from '../../services/api';
 import { response } from 'express';
+import { AuthContext } from '../../contexts/auth';
 
 
-type AuthResponse = {
-
-token: string;
-user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-}
-
-
-}
 
 
 
 export function LoginBox() {
 
+const { signInUrl, user } = useContext(AuthContext);
 
-    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=2e5335f5609c43ba3345`;
-
-
-async function signIn(githubCode: string) {
-
-const response = await api.post<AuthResponse>('authenticate', {
-
-    code: githubCode,
-})
-
-const { token, user} = response.data
-
-
-localStorage.setItem('@dowhile:token', token);
-
-console.log(user)
-
-}
-
-
-useEffect(() => {
-
-const url = window.location.href;
-const hasGithubCode = url.includes('?code=');
-
-if(hasGithubCode) {
-    const [urlWithoutCode, githubCode] = url.split('?code=')
-
-console.log({urlWithoutCode, githubCode});
-
-window.history.pushState({},'', urlWithoutCode);
-
-signIn(githubCode)
-}
-
-}, []);
+console.log(user);
 
     return (
 
